408518f8a15aa73917a39c750c231041dc14a146
Sam Shacter <sshacter@gmail.com>
2024-04-22 04:15:26 -0500

    some interesting issues with the basket. i am able to run some of my tests, and they come through in the console, when i call the array from the console after reloading my tests, it is showing values that i can only see in the automated tests page. I would like to spend more time on this to understand the parts that I am missing now.

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,24 +1,92 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
-let basket = [];
-let item;
-function addItem(item) {
-   
-    console.log('in addItem - you are adding ', item ,' to your basket')
-basket.push(item);
-console.log('The item ', item, ' has been added to your basket!')
-return true;
 
+const maxItems = 5;
+let basket = [];
+
+
+function addItem( item ){
+    console.log('in addItem', item)
+    if (isFull(basket) === true ){
+console.log('Cannot add anymore items to your cart!');
+        return false;
+    }
+    else  
+    basket.push( item );
+    console.log(basket);
+    return true;
 }
 
-console.log('Testing addItem -' , addItem('Apple'));
-console.log('Testing addItem -' , addItem('Peaches'));
-console.log('Testing addItem -' , addItem('Carrots'));
-console.log('Testing addItem -' , addItem('Cottage Cheese'));
+function empty(basket){
+    basket.length = 0;
+    console.log('Reset basket:', basket);
+    return basket;
+}
+
+function listItems(basket){
+for (let i=0; i<basket.length; i++){
+    return basket.join(' , ');
+}
+    
+
+
+}
+function isFull(basket){
+    if (maxItems > basket.length){
+return false;
+    }
+else {
+    if(maxItems <= basket.length){
+    return true;
+}
+}
+}
+
+
+function removeItem(item){
+    console.log(basket);
+    console.log(basket.indexOf(item));
+    if (basket.indexOf(item) !== -1){
+        basket.splice(basket.indexOf(item));
+
+    return basket
+    }
+    console.log( item + " does not exists")
+    return null;
+    }
+   
 
 
 
+
+empty(basket);
+console.log(`Basket is ${basket}`);
+console.log('Adding Strawberries (expect true)', addItem('Strawberries'));
+console.log(`Basket is now ${basket}`);
+console.log(`Basket is ${basket}`);
+console.log('Adding Watermelon (expect true)', addItem('Watermelon'));
+console.log(`Basket is now ${basket}`);
+console.log(`Basket is ${basket}`);
+console.log('Adding Grapes (expect true)', addItem('Grapes'));
+console.log(`Basket is now ${basket}`);
+console.log(`Basket is ${basket}`);
+console.log('Adding Pumpkins (expect true)', addItem('Pumpkins'));
+console.log(`Basket is now ${basket}`);
+console.log(`Basket is ${basket}`);
+console.log('Adding Cherries (expect true)', addItem('Cherries'));
+console.log(`Basket is now ${basket}`);
+console.log(`Basket is ${basket}`);
+console.log('Adding Corn (expect false)', addItem('Corn'));
+console.log(`Basket is still ${basket}`);
+
+
+
+
+console.log('Testing isFull: ', isFull(basket), basket, 'your cart is full!');
+
+console.log('Testing listItems: ', listItems(basket));
+console.log('Testing removeItem: ',  removeItem('Cake'), basket );
 
 
 // DO NOT MODIFY
